71d8904b1078aa583af4d8e102a830de28869db4
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-04-22 21:33:22 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -21,20 +21,20 @@ class App < Sinatra::Base
     "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
   end
   
-  get '/:operation/:number1/:number2' do 
-    operation = params[:operation]
-    case operation
-    when 'add'
-      (params[:number1].to_i) + (params[:number2].to_i)
-    when 'substract'
-      (params[:number1].to_i) - (params[:number2].to_i)
-    when 'multiply'
-      (params[:number1].to_i) * (params[:number2].to_i)
-    when 'divide'
-      (params[:number1].to_i) / (params[:number2].to_i)
-    else 
-      "Unable to perform this operation"
-    end
-  end
+  # get '/:operation/:number1/:number2' do 
+  #   operation = params[:operation]
+  #   case operation
+  #   when 'add'
+  #     (params[:number1].to_i) + (params[:number2].to_i)
+  #   when 'substract'
+  #     (params[:number1].to_i) - (params[:number2].to_i)
+  #   when 'multiply'
+  #     (params[:number1].to_i) * (params[:number2].to_i)
+  #   when 'divide'
+  #     (params[:number1].to_i) / (params[:number2].to_i)
+  #   else 
+  #     "Unable to perform this operation"
+  #   end
+  # end
 
 end
