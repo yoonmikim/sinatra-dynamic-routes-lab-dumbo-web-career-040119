5675d0b36172fb629e08f3f87df176988963eb65
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-04-22 21:29:55 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -25,7 +25,13 @@ class App < Sinatra::Base
     operation = params[:operation]
     case operation
     when 'add'
-    (params[:number1].to_i) + (params[:number2].to_i)
+      (params[:number1].to_i) + (params[:number2].to_i)
+    when 'substract'
+      (params[:number1].to_i) + (params[:number2].to_i)
+    when 'multiply'
+      (params[:number1].to_i) * (params[:number2].to_i)
+    when 'divide'
+      (params[:number1].to_i) / (params[:number2].to_i)
     end
   end
   
